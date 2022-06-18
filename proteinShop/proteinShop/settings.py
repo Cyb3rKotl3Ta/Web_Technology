9d51d333ee08c8980d7a4e6ef1e1c211e72c174c
1 <mistosisi@gmail.com>
2022-06-18 22:04:50 +0300

    db try commit

--- proteinShop/proteinShop/settings.py
+++ proteinShop/proteinShop/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sports_accessories.apps.SportsAccessoriesConfig'
+    'sports_accessories.apps.SportsAccessoriesConfig',
 ]
 
 MIDDLEWARE = [
