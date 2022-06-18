d7d3834f0d2ec8b40f83ec0f870322c84da66e2f
1 <mistosisi@gmail.com>
2022-06-18 22:05:28 +0300

    db try commit2

---
+++ proteinShop/sports_accessories/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class sports_accessories(models.Model):
+    category = models.CharField(max_length=255)
+    cat_id = models.ForeignKey('category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.category
+
+
+class category(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    cost = models.BigIntegerField()
+
+    def __str__(self):
+        return self.title
+
